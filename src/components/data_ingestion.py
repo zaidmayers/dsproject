b3d4d1cc983aec0e3c868e38699d6151f46610b2
Zaid Mayers <zaidmayers@gmail.com>
2025-08-19 23:37:45 -0400

    Added class decorator for future inits

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,7 +3,9 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
 
+@dataclass
 class DataIngestionConfig: 
     train_data = os.path.join('artifacts', 'train.csv')
     test_data = os.path.join('artifacts', 'test.csv')
